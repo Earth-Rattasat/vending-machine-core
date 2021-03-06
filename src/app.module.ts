114f37c9b6c2e65ce7071487ef4c628181c9a832
Earth-Rattasat <80152984+Earth-Rattasat@users.noreply.github.com>
2021-03-06 23:00:08 +0700

    Revert "[+] init prisma, create database and products api"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './products/products.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
